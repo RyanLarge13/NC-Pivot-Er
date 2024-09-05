913600e6dc637d8cbc6486c424f7e4d80b7e3281
= <=>
2024-09-05 12:47:13 -0400

    Create Temp Local
    The temp files are now garunteed to be generated locally and then saved
    to the correct directory after operations are complete
    incase directories or files are stored on a server

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,7 @@ import { electronApp, optimizer, is } from "@electron-toolkit/utils";
 import fs from "fs";
 import readline from "readline";
 import path from "path";
+import os from "os";
 import icon from "../../resources/icon.png?asset";
 
 function createWindow(): void {
@@ -19,20 +20,14 @@ function createWindow(): void {
       sandbox: false
     }
   });
-
   mainWindow.on("ready-to-show", () => {
     mainWindow.show();
   });
-
   mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url);
     return { action: "deny" };
   });
-
   mainWindow.webContents.openDevTools();
-
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
     mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
   } else {
@@ -40,25 +35,16 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId("com.electron");
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on("browser-window-created", (_, window) => {
     optimizer.watchWindowShortcuts(window);
   });
 
-  // IPC file creationg and editing
-
   const editPivot = (filePath, newContent): Promise<void> => {
     return new Promise((res, rej) => {
-      const tmpFilePath = `${filePath}.err`;
+      const homeDir = os.homedir();
+      const tmpFilePath = path.join(homeDir, `${path.basename(filePath)}.err`);
       const input = fs.createReadStream(filePath);
       const output = fs.createWriteStream(tmpFilePath);
       const rl = readline.createInterface({ input });
@@ -72,15 +58,22 @@ app.whenReady().then(() => {
       rl.on("close", () => {
         output.end();
       });
-      output.on("finish", () => {
-        fs.rename(tmpFilePath, filePath, (err) => {
+      output.on("close", () => {
+        const destFilePath = filePath;
+        fs.copyFile(tmpFilePath, destFilePath, (err) => {
           if (err) {
             console.log(err);
             return rej(err);
-          } else {
-            console.log("complete");
-            return res();
           }
+          fs.unlink(tmpFilePath, (err) => {
+            if (err) {
+              console.log(err);
+              return rej(err);
+            } else {
+              console.log("File updated successfully");
+              return res();
+            }
+          });
         });
       });
       rl.on("error", (err) => {
@@ -128,20 +121,12 @@ app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
